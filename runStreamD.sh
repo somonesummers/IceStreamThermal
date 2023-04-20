8fcdce7ea1cf276a70fa7f0b254b30f2113555b9
Paul Summers <psummers@stanford.edu>
2023-04-20 10:09:37 -0700

    run on correct partition

--- runStreamD.sh
+++ runStreamD.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=shearmarginStreamD
 #SBATCH --output=matlab_test.“%j”.out
 #SBATCH --error=matlab_test.“%j”.err
-#SBATCH --partition=normal
+#SBATCH --partition=serc
 #SBATCH --time=00:45:00
 #SBATCH --cpus-per-task=2
 #SBATCH --mem=8G
