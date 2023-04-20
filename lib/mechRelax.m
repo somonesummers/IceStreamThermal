15d2ef0c30e5ae00dfa93cb9a2e2292042c9aecc
Paul Summers <psummers@stanford.edu>
2023-04-19 17:21:16 -0700

    staging version of goldsby that I can mess with more

--- lib/mechRelax.m
+++ lib/mechRelax.m
@@ -5,7 +5,7 @@ mu_old = mu;
 if any(iceRheol==[2,3])
     mu = iceRheology_suckale(T(m*(nT-n)+1:end),tau_E,m,n,rho,g,z);
 elseif iceRheol==4
-    mu = iceRheology_goldsby(T(m*(nT-n)+1:end),tau_E,m,n,rho,g,z);
+    mu = iceRheology_goldsby2000(T(m*(nT-n)+1:end),tau_E,m,n,rho,g,z);
 end
 mu = omegaM*mu + (1-omegaM)*mu_old;
 resM = norm((mu_old-mu)./mu_old);
